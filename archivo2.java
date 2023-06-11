25f8f953df31e0a8ac7caf0f98d03df4ed3c0171
Axel Davila Chavez <axeldavila407@gmail.com>
2023-06-11 10:59:33 -0600

    Eliminando archivo 2

--- archivo2.java
+++
@@ -1,8 +0,0 @@
-public class archivo2 {
-    public static void main(String[] args){
-        String nota = "Amprendiendo a usar Git.";
-        String despedida = "Adios mundo";
-        System.out.println(nota);
-        System.out.println(despedida);
-    }
-}
