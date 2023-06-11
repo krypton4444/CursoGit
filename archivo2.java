ea839970c38f58492b99ab4658a96711d2445bc5
Axel Davila Chavez <axeldavila407@gmail.com>
2023-06-11 10:47:18 -0600

    Agregando saludos

--- archivo2.java
+++ archivo2.java
@@ -1,6 +1,8 @@
 public class archivo2 {
     public static void main(String[] args){
+        String nota = "Amprendiendo a usar Git.";
         String despedida = "Adios mundo";
+        System.out.println(nota);
         System.out.println(despedida);
     }
 }
