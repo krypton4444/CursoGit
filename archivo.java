db7d545e892d13abd5e325a470add025ae777364
Axel Davila Chavez <axeldavila407@gmail.com>
2023-06-11 15:37:41 -0600

    Mostrando status corto

--- archivo.java
+++ archivo.java
@@ -1,6 +1,9 @@
 public class archivo{
     public static void main(String[] args){
         String saludo = "Hola Mundo";
+        String yo = "Quiero ser un Hacker";
+        
         System.out.println(saludo);
+        System.out.println(yo);
     }
 }
---
+++ archivo2.java
@@ -0,0 +1,9 @@
+public class archivo2{
+    public static void main(String[] args) {
+        int num1 = 78;
+        int num2 = 22;
+        int suma = num1 + num2;
+
+        System.out.println("La suma de ambos numeros es: " + suma);
+    }
+}
