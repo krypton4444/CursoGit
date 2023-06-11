d2b69c62bd8bf74833b885805e02f3bc91213143
Axel Davila Chavez <axeldavila407@gmail.com>
2023-06-11 16:04:05 -0600

    actualizando archivo 2

--- archivo2.java
+++ archivo2.java
@@ -3,7 +3,9 @@ public class archivo2{
         int num1 = 78;
         int num2 = 22;
         int suma = num1 + num2;
+        String sistema = "Android";
 
+        System.out.println(sistema);
         System.out.println("La suma de ambos numeros es: " + suma);
     }
 }
